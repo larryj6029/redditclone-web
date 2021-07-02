9f6a299da24e499decf25705ac4723de3c310403
Larry San Nicolas <larryjsannicolas@gmail.com>
2021-07-01 18:34:30 -0700

    2:32:00
    Added the input form for the username and password

---
+++ src/pages/register.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Form, Formik } from "formik";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Box,
+  Button,
+} from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+
+interface registerProps {}
+
+export const Register: React.FC<registerProps> = ({}) => (
+  <Wrapper variant="small">
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <InputField name="username" placeholder="username" label="Username" />
+          <Box mt={4}>
+            <InputField
+              name="password"
+              placeholder="password"
+              label="Password"
+              type="password"
+            />
+          </Box>
+          <Button
+            type="submit"
+            mt={5}
+            isLoading={isSubmitting}
+            colorScheme="teal"
+          >
+            Register
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  </Wrapper>
+);
+
+export default Register;
